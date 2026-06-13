588cf2126e9797056653ebeed75846b6a54ffbe4
gangasv <gangasv@mail.uc.edu>
2026-06-12 23:59:12 -0400

    Completed Lab 05 Task 01

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class ShipCostCalculator {
+    public static void main(String[] args) {
+        //Scanner object to read from the console
+        Scanner in = new Scanner(System.in);
+
+        //Declare variables
+        double itemPrice = 0.0;
+        double shippingCost = 0.0;
+        double totalPrice = 0.0;
+        String trash = ""; // To display invalid text input
+
+        // Ask user for item price
+        System.out.print("Enter the price of your item: $");
+
+        // Check if the input is a double
+        if (in.hasNextDouble()) {
+            itemPrice = in.nextDouble();
+            in.nextLine(); // Clear the newline from the buffer
+
+            // Calculate shipping cost
+            if (itemPrice >= 100.0) {
+                shippingCost = 0.0;
+            }
+            else {
+                shippingCost = itemPrice * 0.02;
+            }
+
+            // Calculate the total price
+            totalPrice = itemPrice + shippingCost;
+
+            //Output
+            System.out.println("Item Price: $" + itemPrice);
+            System.out.println("Shipping Cost: $" + shippingCost);
+            System.out.println("Total Price: $" +  totalPrice);
+        }
+        else {
+            // Not a double, read as a String
+            trash = in.nextLine();
+            System.out.println("Error: '" + trash + "' is not a valid price.");
+            System.out.println("Please run the program again and enter a valid item price");
         }
     }
 }
